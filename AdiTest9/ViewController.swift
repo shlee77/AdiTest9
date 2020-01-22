b65addc90bc65984ddd452fe0bceca7779596157
Lee Sunhak <shlee@neowiz.com>
2020-01-22 16:04:44 +0900

    Initial Commit

---
+++ AdiTest9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdiTest9
+//
+//  Created by 이선학 on 2020/01/22.
+//  Copyright © 2020 이선학. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
