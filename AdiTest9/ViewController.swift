ac49f758d3daf9b3d70259a863c3a4b5f1c9784b
Lee Sunhak <shlee@neowiz.com>
2020-01-22 16:45:41 +0900

    source 수정

--- AdiTest9/ViewController.swift
+++ AdiTest9/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var buttonRVLoad: UIButton!
+    @IBOutlet weak var buttonRVShow: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        print("Adiscope Version = \(String(describing: AdiscopeInterface.sharedInstance().getSDKVersion()))")
     }
 
-
+    @IBAction func respondsToOfferwall(_ sender: Any) {
+        print("respondsToOfferwall")
+    }
+    
+    @IBAction func respondsToLoadRV(_ sender: Any) {
+        print("respondsToLoadRV")
+    }
+    
+    @IBAction func respondsToShowRV(_ sender: Any) {
+        print("respondsToShowRV")
+    }
+    
 }
 
